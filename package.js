db925a7b3acfcc01360dbe3c77509f5c983d63b4
gghez <gregory.ghez@gmail.com>
2016-01-30 01:32:19 +0100

    Release 1.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'gghez:base58',
-    version: '1.0.0',
+    version: '1.1.0',
     // Brief, one-line summary of the package.
     summary: 'Encode / Decode with Base58 algorithm.',
     // URL to the Git repository containing the source code for this package.
